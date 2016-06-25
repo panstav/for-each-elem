0bdf0f2c387dc8b43c009ce7fdcdd4488857059d
Pan Stav Geffen <stavgeffen@gmail.com>
2016-06-25 17:51:42 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = forEachElem;
+
+function forEachElem(identifier, parent, fn){
+
+	// normalize arguments
+	if (typeof(parent) === 'function'){
+		fn = parent;
+		parent = document;
+	}
+
+	// resolve to an iterate-able list of elements
+	// identifier would be either a string pointer to elements that will be queried by for elements
+	// or a list which could be directly iterated with forEach, or something that can transformed to such a list
+	var elements = identifier;
+	if (isString(identifier)) elements = parent.querySelectorAll(identifier);
+	if (!isArray(identifier)) elements = [].slice.call(elements);
+
+	// pass the elements one by one to the given fn
+	elements.forEach(fn);
+}
+
+function isString(str){
+	return typeof(str) === 'string';
+}
+
+function isArray(arr){
+	const toStr = toString || {}.toString;
+	return toStr.call(arr) === '[object Array]';
+}
