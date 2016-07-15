5e903a852b4b38535e1f94b6fa63dc61222c1368
Stav Geffen <stavgeffen@gmail.com>
2016-07-16 00:05:16 +0300

    test(parent): adds test for parent argument
    use of a parent elem to querySelector the lookup collection

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ const forEachElem = require('../');
 
 describe('forEachElem', () => {
 
-	it('Should handle querySelectorAll', done => {
+	it('Should handle querySelectorAll output', done => {
 
 		const html = `
 <ul>
@@ -16,11 +16,7 @@ describe('forEachElem', () => {
 `;
 
 		jsdom.env(html, (err, window) => {
-
-			if (err){
-				console.err(err);
-				console.log(err.stack);
-			}
+			if (err) handle(err);
 
 			global.window = window;
 
@@ -50,11 +46,7 @@ describe('forEachElem', () => {
 `;
 
 		jsdom.env(html, (err, window) => {
-
-			if (err){
-				console.err(err);
-				console.log(err.stack);
-			}
+			if (err) handle(err);
 
 			global.window = window;
 
@@ -73,4 +65,46 @@ describe('forEachElem', () => {
 
 	});
 
+	it('Should be able to look up by parent elem', done => {
+
+		const html = `
+<ul>
+	<li>1</li>
+	<li>2</li>
+	<li>3</li>
+</ul>
+<ol>
+	<li>4</li>
+	<li>5</li>
+	<li>6</li>
+</ol>
+`;
+
+		jsdom.env(html, (err, window) => {
+			if (err) handle(err);
+
+			global.window = window;
+
+			const ol = window.document.getElementsByTagName('ol')[0];
+
+			var text = '';
+			forEachElem('li', ol, li => {
+				text += li.textContent;
+			});
+
+			expect(text).to.be.equal('456');
+
+			// free memory associated with the window
+			window.close();
+
+			done();
+		});
+
+	});
+
 });
+
+function handle(err){
+	console.error(err);
+	console.error(err.stack);
+}
